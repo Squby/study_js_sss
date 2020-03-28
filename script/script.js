d33e0854f5a960896583b713fa78caf1f6d6560f
Vladslav Vasilev <vladis.vasiljew@yandex.ru>
2020-03-29 00:28:47 +0300

    new derctor

---
+++ script/script.js
@@ -0,0 +1,17 @@
+let money = 100000,
+    income  = 'freelance', 
+    addExpenses = 'internet, food, clothes',
+    deposit = true,
+    mission = 1000000,
+    period = 4,
+    budgetDay;
+    typeof(money);
+    console.log(typeof(money));
+    console.log(typeof(income));
+    console.log(typeof(deposit));
+    console.log(addExpenses.length);
+    console.log('Период равен ' + period + ' месяцев');
+    console.log('Цель заработать ' + mission + ' долларов');
+    console.log((addExpenses.toLowerCase()).split(', '));
+    budgetDay = money / 30;
+    console.log('Дневной доход: ', budgetDay = money / 30);
