e76c7b0d56433b83bbdcc5fbb839b44889b592c0
Squby <vladis.vasiljew@yandex.ru>
2020-04-03 09:02:26 +0300

    complited lesson5

--- script/script.js
+++ script/script.js
@@ -1,42 +1,56 @@
 'use strict';
+let isNumber = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+    };
+
+
 let income  = 'freelance', 
-    mission = 1000,
+    mission = 10000,
     period = 4,
     budgetDay,
-    money = prompt('Ваш месячный даход?'),
+    money,
     addExpenses = prompt('Перечислети статьи раходов через запятую'),
-    deposit = confirm('Есть ли у вас депозит в банке?'),
-    expenses1 =  prompt('Введите обязательную статью расходов?'),
-    amount1 = prompt('Во сколько это обойдется?'),
-    expenses2 =  prompt('Введите обязательную статью расходов?'),
-    amount2 = prompt('Во сколько это обойдется?');
-    // countMonth;
-    money = 1*money;
-    amount1 = parseInt(amount1);
-    amount2 = parseInt(amount2);
+    deposit = confirm('Есть ли у вас депозит в банке?');
+    
+    let start = function (){
+         do {
+            money = prompt('Ваш месячный даход?');
+        } while (!isNumber(money));
+    };
+    start();
+    let expenses = [];
+     let getExpensesMonth = function (){
+        let temp = 0,sum = 0;
+        for (let i = 0; i < 2; i++) {
+
+            expenses[i] =  prompt('Введите обязательную статью расходов?', 'еда');
+            do {
+                temp = prompt('Во сколько это обойдется?');}
+                while (!isNumber(temp));
+            sum += +temp;
+        }
+        console.log(sum);
+        return sum;
+    };
+    let expenseAmount = getExpensesMonth();
 
     let showTypeof = function (data) {
         console.log(data, typeof(data));
     };
+    let getAccumulatedMonth = function (){
+        return (money - expenseAmount);
+    };
+    let accumulatedMonth = getAccumulatedMonth();
 
-    function getExpensesMonth (amount1, amount2){
-        return (amount1 + amount2);
-        }
-    console.log(getExpensesMonth (amount1, amount2));
-
-    function getAccumulatedMonth(money, amount1, amount2){
-        return (money - amount1 - amount2);
-    }
-
-    let accumulatedMonth = getAccumulatedMonth(money, amount1, amount2);
-
-    function getTargetMonth (mission, accumulatedMonth){
+    function getTargetMonth (){
         return (Math.ceil(mission/accumulatedMonth));
     }
-    console.log(getTargetMonth (mission, accumulatedMonth));
-
-   
-    // console.log('колличество месяцев = ' + countMonth);
+    if (getTargetMonth () > 0){
+        console.log('Цель будет достигнута за ' + getTargetMonth() + ' месяцев');
+    }   else {
+        console.log('Цель не будет достигнута');
+    }
+    
     showTypeof(money);
     showTypeof(income);
     showTypeof(deposit);
@@ -58,26 +72,3 @@ let income  = 'freelance',
                 return ('Что то пошло не так');
                 }  };
     console.log(getStatusIncome());
-
-
-
-
-// 7) Почистить консоль логи и добавить недостающие, должны остаться:
-
-//  - вызовы функции showTypeOf
-
-//  - Расходы за месяц вызов getExpensesMonth
-
-//  - Вывод возможных расходов в виде массива (addExpenses)
-
-//  - Cрок достижения цели в месяцах (результат вызова функции getTargetMonth) 
-
-//  - Бюджет на день (budgetDay)
-
-//  - вызов функции getStatusIncome
-
-
-// 8) Проверить, чтобы все работало и не было ошибок в консоли
-
-
-// 9) Добавить папку с четвертым уроком в свой репозиторий на GitHub
