c98a463d3339eae279bc0c4bd2311b4888b9b75b
Vladslav Vasilev <vladis.vasiljew@yandex.ru>
2020-05-07 20:36:25 +0300

    complited lesson27

--- script/script.js
+++ script/script.js
@@ -364,35 +364,26 @@ window.addEventListener('DOMContentLoaded', () => {
                         form.appendChild(statusMessage);
                         statusMessage.textContent = loadMessage;
                         const formData = new FormData(form);
-                        let body = {};
+                        //let body = {};
 
-                        const postData = body => new Promise((resolve, reject) => {
-                            const request = new XMLHttpRequest();
-
-                            request.addEventListener('readystatechange', () => {
-                                if (request.readyState !== 4) {
-                                    return;
-                                }
-                                if (request.status === 200) {
-                                    resolve();
-                                } else {
-                                    reject(request.status);
-                                }
-                            });
-
-                            request.open('POST', './server.php');
-                            request.setRequestHeader('Content-Type', 'application/json');
-                            request.send(JSON.stringify(body));
+                        const postData = formData => fetch('./server.php', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: formData
                         });
 
-                        formData.forEach((val, key) => {
-                            body[key] = val;
-                        });
-                        postData(body)
-                            .then(() => statusMessage.textContent = successMessage)
+                        postData(formData)
+                            .then(response => {
+                                if (response.status !== 200) {
+                                    throw new Error('status network not 200');
+                                }
+                                statusMessage.textContent = successMessage;
+                            })
                             .catch(error => {
-                                console.log(error);
                                 statusMessage.textContent = errorMessage;
+                                console.error(error);
                             });
                     });
                 }
