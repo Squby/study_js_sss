7079b33e779dd13e2a65ab6d1f61b2fa1991f8fa
Vladslav Vasilev <vladis.vasiljew@yandex.ru>
2020-05-14 23:38:25 +0300

    Реализовал аккардион списка вопросов

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 'use strict';
 import popupCall from './modules/popupCall';
 import moreTab from './modules/moreTab';
+import accardionTwo from './modules/accardionTwo';
 
+accardionTwo();
 moreTab();
 popupCall();
