de92d77f75def70db42936148bcdd461f401c418
Vladslav Vasilev <vladis.vasiljew@yandex.ru>
2020-05-14 22:06:23 +0300

    Добавил открытие popupDiscount

--- src/modules/popupCall.js
+++ src/modules/popupCall.js
@@ -19,7 +19,25 @@ const popupCall = () => {
             }
         }
     });
-
+    //discount-btn
+    const discountBtn = document.querySelectorAll('.discount-btn'),
+    popupDiscount = document.querySelector('.popup-discount');
+    discountBtn.forEach(element => {
+        element.addEventListener('click', () => {
+            popupDiscount.style.display = 'block';
+        });
+    });
+    popupDiscount.addEventListener('click', event => {
+        let target = event.target;
+        if (target.classList.contains('popup-close')) {
+            popupDiscount.style.display = 'none';
+        } else {
+            target = target.closest('.popup-content');
+            if (!target) {
+                popupDiscount.style.display = 'none';
+            }
+        }
+    });
 
 };
 export default popupCall;
