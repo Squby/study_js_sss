ea05dd39e7cd2da31a3316f23857f469bfa4e992
Vladslav Vasilev <vladis.vasiljew@yandex.ru>
2020-05-14 22:16:10 +0300

    Добавил еще один попап checkBtn

--- src/modules/popupCall.js
+++ src/modules/popupCall.js
@@ -38,6 +38,25 @@ const popupCall = () => {
             }
         }
     });
+    //check-btn
+    const checkBtn = document.querySelector('.check-btn'),
+    popupCheck = document.querySelector('.popup-check');
+        console.log(popupCheck);
+        checkBtn.addEventListener('click', () => {
+            popupCheck.style.display = 'block';
+    });
+    popupCheck.addEventListener('click', event => {
+        let target = event.target;
+        if (target.classList.contains('popup-close')) {
+            popupCheck.style.display = 'none';
+        } else {
+            target = target.closest('.popup-content');
+            if (!target) {
+                popupCheck.style.display = 'none';
+            }
+        }
+    });
+
 
 };
 export default popupCall;
