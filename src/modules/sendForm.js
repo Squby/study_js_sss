a290de2fd3bf3adbb2700fb1ad60cf512a5d0d4e
Vladslav Vasilev <vladis.vasiljew@yandex.ru>
2020-05-19 14:22:42 +0300

    Исправвил баг с пустыми строками

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -45,7 +45,6 @@ const sendForm = () => {
             });
             // Комментарий
                 if (distanceInput.value !== ''){
-                    console.log(onoffswitchCheckbox);
                     body['Количество камер'] = onoffswitchCheckbox.value;
                     body['Диаметр 1-го колодца'] = contentPanelBody[1].children[1].value; 
                     body['Количество колец 1-го колодца'] = contentPanelBody[2].children[1].value;
@@ -53,7 +52,6 @@ const sendForm = () => {
                         body['Диаметр 2-го колодца'] = contentPanelBody[4].children[1].value;
                         body['Количество колец 2-го колодца'] = contentPanelBody[5].children[1].value;
                     }
-                    console.log(myonoffswitchTwo);
                     body['Наличие днища у колодца'] = myonoffswitchTwo.value;
                     body['Растояние до дома'] = distanceInput.value;
                     body['Общая стоимость'] = calcResult.value;
